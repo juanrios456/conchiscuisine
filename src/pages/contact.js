4c154321d8c4982278bd24eac68d0b20f5f04ce4
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-05-09 13:12:37 -0500

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -42,6 +42,7 @@ export function ContactBody(){
           <meta property="og:image" content="https://conchiscuisine.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffiesta-line.72984c46.png&w=3840&q=75"/>
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.conchiscuisine.com/contact"/>
+            <link rel="icon" href="/logo.png" sizes="any"/>
      </Head>
     <Container
       maxW="750px"
