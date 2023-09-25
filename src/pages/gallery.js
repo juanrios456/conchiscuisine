eb61b2f23bd7c41dc3216adbf59b8918e500a154
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-09-25 07:20:46 -0500

    Made the imports for the photos cleaner

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,24 +2,6 @@ import { Container, Heading, Tab, TabList, TabPanel, TabPanels, Tabs, Text } fro
 import styles from "../styles/gallery.module.css";
 import Image from "next/image";
 import Head from "next/head";
-//Event Images
-import cakeEvent from "../../public/gallery/events/cake-event.png";
-import cooking from "../../public/gallery/events/cooking.png";
-import cookingAtEvent from "../../public/gallery/events/cooking-at-event.png";
-import cookingTrompo from "../../public/gallery/events/cooking-trompo.png";
-import cuttingTrompo from "../../public/gallery/events/cutting-trompo.png";
-import fiestaLine from "../../public/gallery/events/fiesta-line.png";
-import fiestaPosing from "../../public/gallery/events/fiesta-posing.png";
-import foodTruck from "../../public/gallery/events/food-truck.png";
-//Food Images
-import burrito from "../../public/gallery/food/burrito-square.png";
-import cakeSquare from "../../public/gallery/food/cake-square.png";
-import cakeSquare2 from "../../public/gallery/food/cake-square2.png";
-import dessertCup from "../../public/gallery/food/dessert-cup.png";
-import quesadilla from "../../public/gallery/food/quesadilla.png";
-import tacosSquare from "../../public/gallery/food/tacos-square.png";
-import tacosWide from "../../public/gallery/food/tacos-wide.png";
-import torta from "../../public/gallery/food/torta.png";
 
 export default function Gallery(){
     
@@ -105,8 +87,8 @@ export default function Gallery(){
                 <Image
                   src={item.src}
                   alt={item.alt}
-                  width="auto"
-                  height="auto"
+                  width={900}
+                  height={800}
                   className={styles.image}
                   data-aos="fade-up"
                   priority
@@ -126,8 +108,8 @@ export default function Gallery(){
                     <Image
                       src={item.src}
                       alt={item.alt}
-                      width="auto"
-                      height="auto"
+                      width={900}
+                      height={800}
                       data-aos="fade-up"
                       className={styles.image}
                       priority
@@ -149,8 +131,8 @@ export default function Gallery(){
                 <Image
                   src={item.src}
                   alt={item.alt}
-                  width="auto"
-                  height="auto"
+                  width={900}
+                  height={800}
                   data-aos="fade-up"
                   className={styles.image}
                   priority
@@ -188,103 +170,103 @@ export function shuffle(array){
 }
 const galleryImages = [
   {
-    src:cakeEvent,
+    src:"/gallery/events/cake-event.png",
     alt: 'Quinceañera set up with over 12 tables with a mini cake for each table',
     ImageType: 'event',
     id: 1,
   },
   {
-    src:cooking,
+    src: "/public/gallery/events/cooking.png",
     alt: 'Cooking fajitas, and chicken, while someone else is setting up the stand.',
     ImageType: 'event',
     id: 2,
 
   },
   {
-    src:cookingAtEvent,
+    src: "/public/gallery/events/cooking-at-event.png",
     alt: 'Cooking meats and preparing for the fiesta mexicana event in Topeka, KS.',
     ImageType: 'event',
     id: 3,
 
   },
   {
-    src:cookingTrompo,
+    src: "/public/gallery/events/cooking-trompo.png",
     alt: 'Cooking a trompo while cooking meats and preparing tacos',
     ImageType: 'event',
     id: 4,
 
   },
   {
-    src:cuttingTrompo,
+    src: "/public/gallery/events/cutting-trompo.png",
     alt: 'Cooking and cutting meat off a spinning meat top',
     ImageType: 'event',
     id: 5,
 
   },
   {
-    src:fiestaLine,
+    src: "/public/gallery/events/fiesta-line.png",
     alt: 'Very long line of people wanting to order food from conchis cuisine',
     ImageType: 'event',
     id: 6,
 
   },
   {
-    src:fiestaPosing,
+    src: "/public/gallery/events/fiesta-posing.png",
     alt: 'Consepcion the founder and chef, waving at the camera',
     ImageType: 'event',
     id: 7,
 
   },
   {
-    src:foodTruck,
+    src: "/public/gallery/events/food-truck.png",
     alt: 'Consepcion set up in her food truck with her trompo and all of her food items',
     ImageType: 'event',
     id: 8,
   },
   {
-    src: burrito,
+    src: "/public/gallery/food/burrito-square.png",
     alt: 'Picture of a burrito',
     ImageType: 'food',
     id: 9,
   },
   {
-    src: cakeSquare,
+    src: "/public/gallery/food/cake-square.png",
     alt: 'Large cake for a quinceañera, 15 year old party',
     ImageType: 'food',
     id: 10,
   },
   {
-    src: cakeSquare2,
+    src: "/public/gallery/food/cake-square2.png",
     alt: '8in. cake with white and purple frosting',
     ImageType: 'food',
     id: 11,
   },
   {
-    src: dessertCup,
+    src: "/public/gallery/food/dessert-cup.png",
     alt: 'Cup of fresas con cremas with conchis cuisine sticker on the side.',
     ImageType: 'food',
     id: 12,
   },
   {
-    src: quesadilla,
+    src: "/public/gallery/food/quesadilla.png",
     alt: 'Quesadillas made of steak, chicken, and fajitas',
     ImageType: 'food',
     id: 13,
   },
   {
-    src: tacosSquare,
+    src: "/public/gallery/food/tacos-square.png",
     alt: '4 pork pastor tacos with limes, grilled onions, and radishes',
     ImageType: 'food',
     id: 14,
   },
   {
-    src: tacosWide,
+    src: "/public/gallery/food/tacos-wide.png",
     alt: '4 pork pastor tacos with limes, grilled onions, and radishes',
     ImageType: 'food',
     id: 15,
   },
   {
-    src: torta,
+    src: "/public/gallery/food/torta.png",
     alt: 'Chicken torta with guacamole, cheese, and a jalapeño on top',
     ImageType: 'food',
     id: 16,
